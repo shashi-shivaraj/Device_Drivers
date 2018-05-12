46a4bad09e6fb0e763f02e514359add1a045813a
shashi-shivaraju <shashi2shivaraj@gmail.com>
2018-05-12 02:56:23 -0400

    code for simple kernel module

---
+++ Minimal_Kernel_2/src/func.h
@@ -0,0 +1,4 @@
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+
+void simple_function(void);
---
+++ Minimal_Kernel_2/src/function.c
@@ -0,0 +1,6 @@
+#include "func.h"
+
+void simple_function(void)
+{
+	printk(KERN_INFO "Hello MOTO from simple function!!!\n");
+}
